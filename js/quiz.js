f520dfd50b8992edc2173927e9110236a3cb9ce9
Praveenanirudh <93002544+Praveenanirudh@users.noreply.github.com>
2022-04-25 16:18:59 +0530

    final  Push

---
+++ js/quiz.js
@@ -0,0 +1,56 @@
+let quiz=[
+    {
+        question:"When did Sachin Tendulkar make his first Test debut?",
+        option:[
+            "1. 10 October, 1988",
+            "2. 11 November, 1988",
+            "3. 20 October 1989",
+            "4. 15 November, 1989",
+
+        ],
+        answer:4,
+    },
+    {
+        question:"What was the name of Sachin Tendulkar’s coach?",
+        option:[
+            "1. Rahul Sinha",
+            "2. Ramakanth Achrekar",
+            "3. Dilip Sardesai",
+            "4. None of the above",
+            
+            
+        ],
+        answer:2,
+    },
+    {
+        question:"When did Sachin Tendulkar receive Arjuna Award?",
+        option:[
+            "1. 1990",
+            "2. 1992",
+            "3. 1994",
+            "4. 1997",   
+        ],
+        answer:3,
+    },
+    {
+        question:" Against which country, Sachin Tendulkar scored his first test century?",
+        option:[
+            "1. England",
+            "2. Pakistan",
+            "3. Australia",
+            "4. New Zealand",   
+        ],
+        answer:1,
+
+    },
+    {
+        question:"When Sachin Tendulkar got married to Anjali?",
+        option:[
+            "1. 24 March, 1995",
+            "2. 24 April, 1995",
+            "3. 24 May, 1995",
+            "4. 24 June, 1995",   
+        ],
+        answer:3,
+    }
+]
